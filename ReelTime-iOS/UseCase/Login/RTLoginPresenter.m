0d2925d2a3c5609f4001af7902ad3ee78b5c2b78
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-01-01 12:59:04 -0500

    Ensure credentials were provided to presenter

--- ReelTime-iOS/UseCase/Login/RTLoginPresenter.m
+++ ReelTime-iOS/UseCase/Login/RTLoginPresenter.m
@@ -1,5 +1,6 @@
 #import "RTLoginPresenter.h"
 #import "RTLoginInteractor.h"
+#import "NSError+RTErrorFactory.h"
 
 @interface RTLoginPresenter ()
 
@@ -22,7 +23,28 @@
 
 - (void)requestedLoginWithUsername:(NSString *)username
                            password:(NSString *)password {
-    
+    NSError *error;
+    BOOL credentialsAreValid = [self validUsername:username
+                                             password:password
+                                                error:&error];
+    if (!credentialsAreValid) {
+        [self loginFailedWithError:error];
+    }
+    else {
+        [self.interactor loginWithUsername:username password:password];
+    }
+}
+
+- (BOOL)validUsername:(NSString *)username
+                password:(NSString *)password
+                   error:(NSError **)outError {
+    if ([username length] == 0) {
+        *outError = [NSError rt_loginErrorWithCode:MissingUsername];
+    }
+    else if ([password length] == 0) {
+        *outError = [NSError rt_loginErrorWithCode:MissingPassword];
+    }
+    return *outError ? NO : YES;
 }
 
 - (void)loginSucceeded {
@@ -30,7 +52,22 @@
 }
 
 - (void)loginFailedWithError:(NSError *)error {
+    NSString *message;
     
+    switch (error.code) {
+        case MissingUsername:
+            message = @"Username is required";
+            break;
+            
+        case MissingPassword:
+            message = @"Password is required";
+            break;
+            
+        default:
+            break;
+    }
+    
+    [self.view showErrorMessage:message];
 }
 
 @end
--- ReelTime-iOSTests/UseCaseTests/LoginTests/RTLoginPresenterSpec.m
+++ ReelTime-iOSTests/UseCaseTests/LoginTests/RTLoginPresenterSpec.m
@@ -13,15 +13,35 @@ describe(@"login presenter", ^{
     __block RTLoginInteractor *interactor;
     __block id<RTLoginView> view;
     
+    __block NSString *username;
+    __block NSString *password;
+    
     beforeEach(^{
         view = mockProtocol(@protocol(RTLoginView));
         interactor = mock([RTLoginInteractor class]);
         
         presenter = [[RTLoginPresenter alloc] initWithView:view
                                                 interactor:interactor];
+        
+        username = @"someone";
+        password = @"secret";
     });
     
     describe(@"login requested", ^{
+        it(@"should fail when username is missing", ^{
+            [presenter requestedLoginWithUsername:@"" password:password];
+            [verify(view) showErrorMessage:@"Username is required"];
+        });
+        
+        it(@"should fail when password is missing", ^{
+            [presenter requestedLoginWithUsername:username password:@""];
+            [verify(view) showErrorMessage:@"Password is required"];
+        });
+        
+        it(@"should pass credentials to interactor", ^{
+            [presenter requestedLoginWithUsername:username password:password];
+            [verify(interactor) loginWithUsername:username password:password];
+        });
     });
     
 });
