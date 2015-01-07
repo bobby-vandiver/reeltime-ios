f04bfd1a0b5415e7d6ad7f949e4ebe733ec2d179
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-01-06 20:48:22 -0500

    Add tests for current user store

---
+++ ReelTime-iOSTests/StoreTests/RTCurrentUserStoreSpec.m
@@ -0,0 +1,32 @@
+#import "RTTestCommon.h"
+#import "RTFakeKeyChainWrapper.h"
+
+#import "RTCurrentUserStore.h"
+
+SpecBegin(RTCurrentUserStore)
+
+describe(@"current user store", ^{
+   
+    __block RTCurrentUserStore *store;
+    __block RTKeyChainWrapper *keyChainWrapper;
+    
+    before(^{
+        keyChainWrapper = [[RTFakeKeyChainWrapper alloc] init];
+        store = [[RTCurrentUserStore alloc] initWithKeyChainWrapper:keyChainWrapper];
+    });
+    
+    it(@"should return nil when there is no current user", ^{
+        NSString *username = [store loadCurrentUsernameWithError:nil];
+        expect(username).to.beNil();
+    });
+    
+    it(@"should return current username", ^{
+        BOOL success = [store storeCurrentUsername:@"someone" error:nil];
+        expect(success).to.beTruthy();
+        
+        NSString *username = [store loadCurrentUsernameWithError:nil];
+        expect(username).to.equal(@"someone");
+    });
+});
+
+SpecEnd
