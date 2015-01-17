8915ffb088dd72d965cd777e6857aeff69ebf1df
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-01-17 12:57:49 -0500

    Add init method for wireframe

--- ReelTime-iOS/UseCase/Login/RTLoginWireframe.h
+++ ReelTime-iOS/UseCase/Login/RTLoginWireframe.h
@@ -1,7 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@class RTLoginPresenter;
+@class RTLoginViewController;
+
 @interface RTLoginWireframe : NSObject
+
+- (instancetype)initWithPresenter:(RTLoginPresenter *)presenter
+                   viewController:(RTLoginViewController *)viewController;
 
 - (void)presentLoginInterfaceFromWindow:(UIWindow *)window;
 
--- ReelTime-iOS/UseCase/Login/RTLoginWireframe.m
+++ ReelTime-iOS/UseCase/Login/RTLoginWireframe.m
@@ -1,7 +1,25 @@
 #import "RTLoginWireframe.h"
+#import "RTLoginPresenter.h"
 #import "RTLoginViewController.h"
 
+@interface RTLoginWireframe ()
+
+@property RTLoginPresenter *presenter;
+@property RTLoginViewController *viewController;
+
+@end
+
 @implementation RTLoginWireframe
+
+- (instancetype)initWithPresenter:(RTLoginPresenter *)presenter
+                   viewController:(RTLoginViewController *)viewController {
+    self = [super init];
+    if (self) {
+        self.presenter = presenter;
+        self.viewController = viewController;
+    }
+    return self;
+}
 
 - (void)presentLoginInterfaceFromWindow:(UIWindow *)window {
 }
