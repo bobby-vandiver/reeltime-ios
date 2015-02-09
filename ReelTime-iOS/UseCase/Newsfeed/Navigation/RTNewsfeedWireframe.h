63e528ff1f0978d2279ebfc6bfb350a99e968886
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-02-08 20:54:47 -0500

    Define wireframe interface to present activity details

--- ReelTime-iOS/UseCase/Newsfeed/Navigation/RTNewsfeedWireframe.h
+++ ReelTime-iOS/UseCase/Newsfeed/Navigation/RTNewsfeedWireframe.h
@@ -1,5 +1,15 @@
 #import <Foundation/Foundation.h>
 
+@class RTUser;
+@class RTReel;
+@class RTVideo;
+
 @interface RTNewsfeedWireframe : NSObject
+
+- (void)presentUser:(RTUser *)user;
+
+- (void)presentReel:(RTReel *)reel;
+
+- (void)presentVideo:(RTVideo *)video;
 
 @end
--- ReelTime-iOS/UseCase/Newsfeed/Navigation/RTNewsfeedWireframe.m
+++ ReelTime-iOS/UseCase/Newsfeed/Navigation/RTNewsfeedWireframe.m
@@ -1,5 +1,21 @@
 #import "RTNewsfeedWireframe.h"
 
+#import "RTUser.h"
+#import "RTReel.h"
+#import "RTVideo.h"
+
 @implementation RTNewsfeedWireframe
+
+- (void)presentUser:(RTUser *)user {
+    
+}
+
+- (void)presentReel:(RTReel *)reel {
+    
+}
+
+- (void)presentVideo:(RTVideo *)video {
+    
+}
 
 @end
