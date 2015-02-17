4103db059169195b9abadad27b55eaa59c68069d
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-02-16 20:02:17 -0500

    Start on join audience and follow user support

--- ReelTime-iOS/Common/Client/RTRestAPI.h
+++ ReelTime-iOS/Common/Client/RTRestAPI.h
@@ -3,3 +3,5 @@
 extern NSString *const API_TOKEN_ENDPOINT;
 extern NSString *const API_ACCOUNT_REGISTRATION_ENDPOINT;
 extern NSString *const API_NEWSFEED_ENDPOINT;
+extern NSString *const API_JOIN_REEL_AUDIENCE_ENDPOINT;
+extern NSString *const API_FOLLOW_USER_ENDPOINT;
--- ReelTime-iOS/Common/Client/RTRestAPI.m
+++ ReelTime-iOS/Common/Client/RTRestAPI.m
@@ -3,3 +3,5 @@
 NSString *const API_TOKEN_ENDPOINT = @"oauth/token";
 NSString *const API_ACCOUNT_REGISTRATION_ENDPOINT = @"api/account";
 NSString *const API_NEWSFEED_ENDPOINT = @"api/newsfeed";
+NSString *const API_JOIN_REEL_AUDIENCE_ENDPOINT = @"api/reels/%@/audience";
+NSString *const API_FOLLOW_USER_ENDPOINT = @"api/users/%@/follow";
