0b1fde1e38a22302e8197f049011088e9b32f8f5
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-03-13 22:01:38 -0400

    Remove stale comment

--- ReelTime-iOS/UseCase/Newsfeed/Persistence/RTNewsfeedDataManager.m
+++ ReelTime-iOS/UseCase/Newsfeed/Persistence/RTNewsfeedDataManager.m
@@ -10,8 +10,6 @@
 - (void)retrievePage:(NSUInteger)page
             callback:(void (^)(NSArray *items))callback {
     
-    // TODO: Ensure listPage is instance of RTNewsfeed
-
     NewsfeedCallback successCallback = ^(RTNewsfeed *newsfeed) {
         callback(newsfeed.activities);
     };
