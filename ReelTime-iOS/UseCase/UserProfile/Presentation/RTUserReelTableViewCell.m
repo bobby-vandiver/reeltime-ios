bb4601929e561755cdfa934d2ec5d91c49690608
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-04-12 23:38:21 -0400

    Add dedicated thumbnail view cell class

--- ReelTime-iOS/UseCase/UserProfile/Presentation/RTUserReelTableViewCell.m
+++ ReelTime-iOS/UseCase/UserProfile/Presentation/RTUserReelTableViewCell.m
@@ -4,6 +4,8 @@
 #import "RTBrowseVideosPresenter.h"
 #import "RTVideoDescription.h"
 
+#import "RTVideoThumbnailTableViewCell.h"
+
 #import "RTPagedListViewScrollHandler.h"
 #import "RTLogging.h"
 
@@ -19,14 +21,6 @@
 
 @implementation RTUserReelTableViewCell
 
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        self.frame = CGRectMake(0, 0, 320, 416);
-    }
-    return self;
-}
-
 - (void)configureWithVideosPresenter:(RTBrowseVideosPresenter *)videosPresenter {
     [self registerDataSource];
     [self registerTableView];
@@ -38,19 +32,10 @@
 
 - (void)registerDataSource {
     
-    ConfigureCellBlock configureBlock = ^(UITableViewCell *cell, RTVideoDescription *description) {
-        cell.frame = CGRectMake(cell.frame.origin.x, cell.frame.origin.y, 75, 75);
-        cell.transform = CGAffineTransformMakeRotation(M_PI * 0.5);
-        
-        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        
-        dispatch_async(queue, ^{
-            UIImage *thumbnail = [UIImage imageWithData:description.thumbnailData];
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                cell.imageView.image = thumbnail;
-            });
-        });        
+    ConfigureCellBlock configureBlock = ^(RTVideoThumbnailTableViewCell *cell, RTVideoDescription *description) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            cell.thumbnail.image = [UIImage imageWithData:description.thumbnailData];
+        });
     };
     
     self.dataSource = [[RTMutableArrayDataSource alloc] initWithItems:@[]
@@ -59,32 +44,27 @@
 }
 
 - (void)registerTableView {
-    self.tableView = [self createHorizontalTableView];
-    [self addSubview:self.tableView];
-}
-
-- (UITableView *)createHorizontalTableView {
     UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 75, 375)
                                                           style:UITableViewStylePlain];
     
     tableView.showsVerticalScrollIndicator = NO;
     tableView.showsHorizontalScrollIndicator = NO;
-
+    
     tableView.transform = CGAffineTransformMakeRotation(-M_PI * 0.5);
     tableView.frame = CGRectMake(0, 0, 375, 75);
-    
     
     tableView.rowHeight = 75;
     
     tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
     tableView.separatorColor = [UIColor clearColor];
-        
+    
     tableView.dataSource = self.dataSource;
     tableView.delegate = self;
     
-    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UserReelVideoCell"];
-    
-    return tableView;
+    [tableView registerClass:[RTVideoThumbnailTableViewCell class] forCellReuseIdentifier:@"UserReelVideoCell"];
+
+    self.tableView = tableView;
+    [self addSubview:tableView];
 }
 
 - (void)registerScrollHandler {
@@ -102,7 +82,7 @@
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    [self.scrollHandler handleScrollForTableView:self.tableView withPresenter:self.videosPresenter];
+//    [self.scrollHandler handleScrollForTableView:self.tableView withPresenter:self.videosPresenter];
 }
 
 @end
---
+++ ReelTime-iOS/UseCase/UserProfile/Presentation/RTVideoThumbnailTableViewCell.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface RTVideoThumbnailTableViewCell : UITableViewCell
+
+@property UIImageView *thumbnail;
+
+@end
