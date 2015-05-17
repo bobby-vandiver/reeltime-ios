90a76a04d770683e378263d3cbc9a76531d5d205
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-05-17 10:55:08 -0400

    Start on reset password validator

--- ReelTime-iOS/UseCase/ResetPassword/Domain/RTResetPasswordValidator.m
+++ ReelTime-iOS/UseCase/ResetPassword/Domain/RTResetPasswordValidator.m
@@ -1,4 +1,6 @@
 #import "RTResetPasswordValidator.h"
+#import "RTResetPasswordError.h"
+#import "RTErrorFactory.h"
 
 @implementation RTResetPasswordValidator
 
@@ -6,7 +8,22 @@
             username:(NSString *)username
          newPassword:(NSString *)newPassword
               errors:(NSArray *__autoreleasing *)errors {
-    return NO;
+    BOOL valid = YES;
+    NSMutableArray *errorContainer = [NSMutableArray array];
+
+    [self validateCode:code errors:errorContainer];
+    [self validateUsername:username errors:errorContainer];
+    [self validatePassword:newPassword errors:errorContainer];
+    
+    if (errorContainer.count > 0) {
+        valid = NO;
+        
+        if (errors) {
+            *errors = errorContainer;
+        }
+    }
+    
+    return valid;
 }
 
 - (BOOL)validateCode:(NSString *)code
@@ -15,6 +32,33 @@
        newClientName:(NSString *)newClientName
               errors:(NSArray *__autoreleasing *)errors {
     return NO;
+}
+
+- (void)validateCode:(NSString *)code
+              errors:(NSMutableArray *)errors {
+    if (code.length == 0) {
+        [self addResetErrorCode:RTResetPasswordErrorMissingResetCode toErrors:errors];
+    }
+}
+
+- (void)validateUsername:(NSString *)username
+                  errors:(NSMutableArray *)errors {
+    if (username.length == 0) {
+        [self addResetErrorCode:RTResetPasswordErrorMissingUsername toErrors:errors];
+    }
+}
+
+- (void)validatePassword:(NSString *)password
+                  errors:(NSMutableArray *)errors {
+    if (password.length == 0) {
+        [self addResetErrorCode:RTResetPasswordErrorMissingNewPassword toErrors:errors];
+    }
+}
+
+- (void)addResetErrorCode:(RTResetPasswordError)code
+                 toErrors:(NSMutableArray *)errors {
+    NSError *error = [RTErrorFactory resetPasswordErrorWithCode:code];
+    [errors addObject:error];
 }
 
 @end
---
+++ ReelTime-iOSTests/UseCaseTests/ResetPasswordTests/RTResetPasswordValidatorSpec.m
@@ -0,0 +1,65 @@
+#import "RTTestCommon.h"
+
+#import "RTResetPasswordValidator.h"
+#import "RTResetPasswordError.h"
+
+#import "RTErrorFactory.h"
+
+SpecBegin(RTResetPasswordValidator)
+
+describe(@"reset password validator", ^{
+    __block RTResetPasswordValidator *validator;
+
+    // TODO: Move this to test common and update other tests
+    NSString *const resetCode = @"reset";
+    
+    NSString *const CODE_KEY = @"code";
+    NSString *const USERNAME_KEY = @"username";
+    NSString *const NEW_PASSWORD_KEY = @"newPassword";
+    NSString *const CLIENT_NAME_KEY = @"clientName";
+
+    void (^expectErrorForBadParametersForRegisteredClient)(NSDictionary *parameters, NSArray *expectedErrorCodes) =
+    ^(NSDictionary *parameters, NSArray *expectedErrorCodes) {
+        
+        NSString *codeParam = parameters[CODE_KEY];
+        NSString *usernameParam = parameters[USERNAME_KEY];
+        NSString *newPasswordParam = parameters[NEW_PASSWORD_KEY];
+        
+        NSArray *errors;
+        BOOL valid = [validator validateCode:getParameterOrDefault(codeParam, resetCode)
+                                    username:getParameterOrDefault(usernameParam, username)
+                                 newPassword:getParameterOrDefault(newPasswordParam, password)
+                                      errors:&errors];
+        
+        expect(valid).to.beFalsy();
+        expect(errors).to.haveACountOf(expectedErrorCodes.count);
+        
+        for (NSNumber *errorCode in expectedErrorCodes) {
+            NSError *expected = [RTErrorFactory resetPasswordErrorWithCode:[errorCode integerValue]];
+            expect(errors).to.contain(expected);
+        }
+    };
+    
+    beforeEach(^{
+        validator = [[RTResetPasswordValidator alloc] init];
+    });
+    
+    context(@"missing parameters", ^{
+        it(@"blank code", ^{
+            expectErrorForBadParametersForRegisteredClient(@{CODE_KEY: BLANK}, @[@(RTResetPasswordErrorMissingResetCode)]);
+            expectErrorForBadParametersForRegisteredClient(@{CODE_KEY: [NSNull null]}, @[@(RTResetPasswordErrorMissingResetCode)]);
+        });
+        
+        it(@"blank username", ^{
+            expectErrorForBadParametersForRegisteredClient(@{USERNAME_KEY: BLANK}, @[@(RTResetPasswordErrorMissingUsername)]);
+            expectErrorForBadParametersForRegisteredClient(@{USERNAME_KEY: [NSNull null]}, @[@(RTResetPasswordErrorMissingUsername)]);
+        });
+        
+        it(@"blank password", ^{
+            expectErrorForBadParametersForRegisteredClient(@{NEW_PASSWORD_KEY: BLANK}, @[@(RTResetPasswordErrorMissingNewPassword)]);
+            expectErrorForBadParametersForRegisteredClient(@{NEW_PASSWORD_KEY: [NSNull null]}, @[@(RTResetPasswordErrorMissingNewPassword)]);
+        });
+    });
+});
+
+SpecEnd
