1242e81949ecbf3731dc0b0006fc52d6cf8faf6d
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-06-14 09:49:38 -0400

    Remove useless comment header

--- ReelTime-iOS/main.m
+++ ReelTime-iOS/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  ReelTime-iOS
-//
-//  Created by Bobby Vandiver on 12/26/14.
-//  Copyright (c) 2014 ReelTime. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "RTAppDelegate.h"
 
