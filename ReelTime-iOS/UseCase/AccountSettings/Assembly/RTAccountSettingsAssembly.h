b2376ea7c2b607706adf66b5a24a0f75d4087dba
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-06-21 21:32:10 -0400

    Wire up account settings module

--- ReelTime-iOS/UseCase/AccountSettings/Assembly/RTAccountSettingsAssembly.h
+++ ReelTime-iOS/UseCase/AccountSettings/Assembly/RTAccountSettingsAssembly.h
@@ -1,11 +1,31 @@
 #import "TyphoonAssembly.h"
 
+@class RTChangeDisplayNameAssembly;
+@class RTChangePasswordAssembly;
+
+@class RTConfirmAccountAssembly;
+@class RTManageDevicesAssembly;
+
+@class RTApplicationAssembly;
+
 @class RTAccountSettingsWireframe;
 @class RTAccountSettingsViewController;
 @class RTAccountSettingsPresenter;
 
 @interface RTAccountSettingsAssembly : TyphoonAssembly
 
+@property (nonatomic, strong, readonly) RTChangeDisplayNameAssembly *changeDisplayNameAssembly;
+@property (nonatomic, strong, readonly) RTChangePasswordAssembly *changePasswordAssembly;
+
+@property (nonatomic, strong, readonly) RTConfirmAccountAssembly *confirmAccountAssembly;
+@property (nonatomic, strong, readonly) RTManageDevicesAssembly *manageDevicesAssembly;
+
+@property (nonatomic, strong, readonly) RTApplicationAssembly *applicationAssembly;
+
 - (RTAccountSettingsWireframe *)accountSettingsWireframe;
+
+- (RTAccountSettingsViewController *)accountSettingsViewController;
+
+- (RTAccountSettingsPresenter *)accountSettingsPresenter;
 
 @end
--- ReelTime-iOS/UseCase/AccountSettings/Assembly/RTAccountSettingsAssembly.m
+++ ReelTime-iOS/UseCase/AccountSettings/Assembly/RTAccountSettingsAssembly.m
@@ -1,9 +1,47 @@
 #import "RTAccountSettingsAssembly.h"
 
+#import "RTChangeDisplayNameAssembly.h"
+#import "RTChangePasswordAssembly.h"
+
+#import "RTConfirmAccountAssembly.h"
+#import "RTManageDevicesAssembly.h"
+
+#import "RTApplicationAssembly.h"
+
+#import "RTAccountSettingsWireframe.h"
+#import "RTAccountSettingsViewController.h"
+#import "RTAccountSettingsPresenter.h"
+
 @implementation RTAccountSettingsAssembly
 
 - (RTAccountSettingsWireframe *)accountSettingsWireframe {
-    return nil;
+    return [TyphoonDefinition withClass:[RTAccountSettingsWireframe class] configuration:^(TyphoonDefinition *definition) {
+        [definition injectMethod:@selector(initWithViewController:changeDisplayNameWireframe:changePasswordWireframe:confirmAccountWireframe:manageDevicesWireframe:applicationWireframe:)
+                      parameters:^(TyphoonMethod *method) {
+                          [method injectParameterWith:[self accountSettingsViewController]];
+                          [method injectParameterWith:[self.changeDisplayNameAssembly changeDisplayNameWireframe]];
+                          [method injectParameterWith:[self.changePasswordAssembly changePasswordWireframe]];
+                          [method injectParameterWith:[self.confirmAccountAssembly confirmAccountWireframe]];
+                          [method injectParameterWith:[self.manageDevicesAssembly manageDevicesWireframe]];
+                          [method injectParameterWith:[self.applicationAssembly applicationWireframe]];
+        }];
+    }];
+}
+
+- (RTAccountSettingsViewController *)accountSettingsViewController {
+    return [TyphoonDefinition withClass:[RTAccountSettingsViewController class] configuration:^(TyphoonDefinition *definition) {
+        [definition useInitializer:@selector(viewControllerWithPresenter:) parameters:^(TyphoonMethod *initializer) {
+            [initializer injectParameterWith:[self accountSettingsPresenter]];
+        }];
+    }];
+}
+
+- (RTAccountSettingsPresenter *)accountSettingsPresenter {
+    return [TyphoonDefinition withClass:[RTAccountSettingsPresenter class] configuration:^(TyphoonDefinition *definition) {
+        [definition injectMethod:@selector(initWithWireframe:) parameters:^(TyphoonMethod *method) {
+            [method injectParameterWith:[self accountSettingsWireframe]];
+        }];
+    }];
 }
 
 @end
