0d937a2c37a75d096372323607370ac3192e0f0a
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-09-27 20:33:42 -0400

    Start defining tests for renegotiation

--- ReelTime-iOSTests/ClientTests/RTAuthenticationAwareHTTPClientDelegateSpec.m
+++ ReelTime-iOSTests/ClientTests/RTAuthenticationAwareHTTPClientDelegateSpec.m
@@ -1,10 +1,15 @@
 #import "RTTestCommon.h"
+#import "RTCallbackTestExpectation.h"
+
 #import "RTAuthenticationAwareHTTPClientDelegate.h"
 
 #import "RTAPIClient.h"
 #import "RTCurrentUserService.h"
 
+#import "RTClientCredentials.h"
+
 #import "RTOAuth2Token.h"
+#import "RTOAuth2TokenError.h"
 
 SpecBegin(RTAuthenticationAwareHTTPClientDelegate)
 
@@ -13,20 +18,24 @@ describe(@"http client delegate", ^{
     
     __block RTAPIClient *client;
     __block RTCurrentUserService *currentUserService;
-    
+
+    __block RTOAuth2Token *token;
+    __block RTClientCredentials *clientCredentials;
+
     beforeEach(^{
         client = mock([RTAPIClient class]);
         currentUserService = mock([RTCurrentUserService class]);
         
         delegate = [[RTAuthenticationAwareHTTPClientDelegate alloc] initWithAPIClient:client
                                                                    currentUserService:currentUserService];
+
+        token = [[RTOAuth2Token alloc] init];
+        clientCredentials = [[RTClientCredentials alloc] initWithClientId:clientId
+                                                             clientSecret:clientSecret];
     });
     
     describe(@"getting access token for current user", ^{
-        __block RTOAuth2Token *token;
-        
         beforeEach(^{
-            token = [[RTOAuth2Token alloc] init];
             token.accessToken = @"access-token";
 
             [given([currentUserService tokenForCurrentUser]) willReturn:token];
@@ -39,6 +48,47 @@ describe(@"http client delegate", ^{
         it(@"should delegate to current user service to get token", ^{
             NSString *accessToken = [delegate accessTokenForCurrentUser];
             expect(accessToken).to.equal(@"access-token");
+        });
+    });
+    
+    describe(@"renegotiating token", ^{
+        __block RTCallbackTestExpectation *renegotiationSuccess;
+        __block RTCallbackTestExpectation *renegotiationFailure;
+        
+        __block MKTArgumentCaptor *successCaptor;
+        __block MKTArgumentCaptor *failureCaptor;
+        
+        beforeEach(^{
+            renegotiationSuccess = [RTCallbackTestExpectation noArgsCallbackTestExpectation];
+            renegotiationFailure = [RTCallbackTestExpectation noArgsCallbackTestExpectation];
+            
+            successCaptor = [[MKTArgumentCaptor alloc] init];
+            failureCaptor = [[MKTArgumentCaptor alloc] init];
+            
+            [given([currentUserService clientCredentialsForCurrentUser]) willReturn:clientCredentials];
+        });
+        
+        context(@"access token has expired", ^{
+            __block RTOAuth2TokenError *tokenError;
+            
+            beforeEach(^{
+                tokenError = [[RTOAuth2TokenError alloc] initWithErrorCode:@"invalid_token"
+                                                          errorDescription:@"Access token expired: access-token"];
+                
+                [delegate renegotiateTokenDueToTokenError:tokenError
+                                                  success:renegotiationSuccess.noArgsCallback
+                                                  failure:renegotiationFailure.noArgsCallback];
+                
+                [verify(client) refreshToken:token
+                       withClientCredentials:clientCredentials
+                                     success:[successCaptor capture]
+                                     failure:[failureCaptor capture]];
+                
+                [renegotiationSuccess expectCallbackNotExecuted];
+                [renegotiationFailure expectCallbackNotExecuted];
+            });
+
+            // TODO: On successful renegotiation, new token should be stored
         });
     });
 });
