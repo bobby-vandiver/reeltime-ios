38264412fc9edbc31b87d3f3bdb1d2bec300446d
Bobby Vandiver <bobby.vandiver88@gmail.com>
2015-10-01 23:26:22 -0400

    Use NSCondition to handle waiting for renegotiation

--- ReelTime-iOS/Common/Client/RTAuthenticationAwareHTTPClientDelegate.m
+++ ReelTime-iOS/Common/Client/RTAuthenticationAwareHTTPClientDelegate.m
@@ -14,12 +14,10 @@ static NSString *const RTTokenRenegotiationFinished = @"RTTokenRenegotiationFini
 @property RTAPIClient *client;
 @property RTCurrentUserService *currentUserService;
 
-@property NSLock *renegotiationLock;
+@property NSCondition *condition;
 
 @property (nonatomic) BOOL renegotiationInProgress;
 @property (nonatomic) BOOL renegotiationSucceeded;
-
-@property (nonatomic) NSInteger renegotiationCounter;
 
 @end
 
@@ -32,12 +30,10 @@ static NSString *const RTTokenRenegotiationFinished = @"RTTokenRenegotiationFini
         self.client = client;
         self.currentUserService = currentUserService;
         
-        self.renegotiationLock = [[NSLock alloc] init];
-
+        self.condition = [[NSCondition alloc] init];
+        
         self.renegotiationInProgress = NO;
         self.renegotiationSucceeded = NO;
-        
-        self.renegotiationCounter = 0;
     }
     return self;
 }
@@ -51,46 +47,30 @@ static NSString *const RTTokenRenegotiationFinished = @"RTTokenRenegotiationFini
 - (void)renegotiateTokenDueToTokenError:(RTOAuth2TokenError *)tokenError
                                 success:(NoArgsCallback)success
                                 failure:(NoArgsCallback)failure {
-
+    
     DDLogDebug(@"entering renegotiateTokenDueToTokenError:success:failure");
-
-    [self incrementCounter];
     
-//    __block BOOL renegotiationInProgress = NO;
-//    
-//    [self performCriticalBlock:^{
-//        renegotiationInProgress = self.renegotiationInProgress;
-//    }];
-
-    BOOL renegotiationInProgress = [self performCriticalBoolBlock:^BOOL{
-        return self.renegotiationInProgress;
-    }];
+    [self.condition lock];
+    BOOL renegotiationInProgress = self.renegotiationInProgress;
+    [self.condition unlock];
     
-
     DDLogDebug(@"renegotiationInProgress before branch = %@", (renegotiationInProgress ? @"YES" : @"NO"));
     
     if (renegotiationInProgress) {
-        [self waitUntilRenegotiationIsFinished];
-
-//        __block BOOL renegotiationSucceeded;
-//    
-//        [self performCriticalBlock:^{
-//            renegotiationSucceeded = self.renegotiationSucceeded;
-//        }];
-        
-        BOOL renegotiationSucceeded = [self performCriticalBoolBlock:^BOOL{
-            return self.renegotiationSucceeded;
+        [self waitUntilRenegotiationIsFinished:^{
+            
+            [self.condition lock];
+            BOOL renegotiationSucceeded = self.renegotiationSucceeded;
+            [self.condition unlock];
+            
+            if (renegotiationSucceeded) {
+                DDLogDebug(@"Waited on successful renegotiation.");
+                success();
+            }
+            else {
+                DDLogDebug(@"Waited for failed renegotiation. Depending on failure to kick off recovery mechanism...");
+            }
         }];
-        
-        [self decrementCounter];
-
-        if (renegotiationSucceeded) {
-            DDLogDebug(@"Waited on successful renegotiation.");
-            success();
-        }
-        else {
-            DDLogDebug(@"Waited for failed renegotiation. Depending on failure to kick off recovery mechanism...");
-        }
     }
     else {
         [self startRenegotiation];
@@ -105,104 +85,61 @@ static NSString *const RTTokenRenegotiationFinished = @"RTTokenRenegotiationFini
     }
 }
 
-- (BOOL)performCriticalBoolBlock:(BOOL (^)())block {
-    [self.renegotiationLock lock];
-    BOOL result = block();
-    [self.renegotiationLock unlock];
-    return result;
-}
-
-- (void)performCriticalBlock:(void (^)())block {
-    [self.renegotiationLock lock];
-    block();
-    [self.renegotiationLock unlock];
-}
-
-- (void)waitUntilRenegotiationIsFinished {
-    DDLogDebug(@"waiting until renegotiation is finished...");
+- (void)waitUntilRenegotiationIsFinished:(NoArgsCallback)callback {
     
-    BOOL renegotiationInProgress;
-
-    do {
-        renegotiationInProgress = [self performCriticalBoolBlock:^{
-            return self.renegotiationInProgress;
-        }];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self.condition lock];
         
-        if (renegotiationInProgress) {
-            DDLogDebug(@"sleeping for one second");
-            sleep(1);
+        DDLogDebug(@"waiting until renegotiation is finished...");
+        
+        while (self.renegotiationInProgress) {
+            [self.condition wait];
         }
         
-    } while (renegotiationInProgress);
-}
-
-- (void)incrementCounter {
-    [self performCriticalBlock:^{
-        DDLogDebug(@"incrementing counter");
-        self.renegotiationCounter++;
-    }];
-}
-
-- (void)decrementCounter {
-    [self performCriticalBlock:^{
-        DDLogDebug(@"decrementing counter");
-        self.renegotiationCounter--;
+        [self.condition unlock];
         
-        if (self.renegotiationCounter == 0) {
-            DDLogDebug(@"Resetting renegotiation tracking variables");
-            self.renegotiationSucceeded = NO;
-        }
-    }];
+        callback();
+    });
 }
 
 - (void)startRenegotiation {
-    [self performCriticalBlock:^{
-        DDLogDebug(@"start renegotiation");
-        self.renegotiationInProgress = YES;
-    }];
+    [self.condition lock];
+    
+    DDLogDebug(@"start renegotiation");
+    self.renegotiationInProgress = YES;
+    
+    [self.condition unlock];
 }
 
-- (void)renegotiationFinished {
-    [self performCriticalBlock:^{
-        DDLogDebug(@"renegotiation finished");
-        self.renegotiationInProgress = NO;
-    }];
-}
-
-- (void)renegotiationFinishedSuccessfully {
-    [self performCriticalBlock:^{
-        DDLogDebug(@"renegotiation finished successfully");
-        self.renegotiationInProgress = NO;
-        self.renegotiationSucceeded = YES;
-    }];
+- (void)renegotiationFinished:(BOOL)success {
+    [self.condition lock];
+    
+    DDLogDebug(@"@renegotiation finished -- success = %@", (success ? @"YES" : @"NO"));
+    
+    self.renegotiationInProgress = NO;
+    self.renegotiationSucceeded = success;
+    
+    [self.condition signal];
+    [self.condition unlock];
 }
 
 - (TokenCallback)tokenSuccessCallbackWithSuccess:(NoArgsCallback)success
                                          failure:(NoArgsCallback)failure {
     return ^(RTOAuth2Token *token) {
         DDLogDebug(@"succesful refresh -- token = %@", token);
-
+        
         BOOL stored = [self.currentUserService storeTokenForCurrentUser:token];
-
-        if (stored) {
-            [self renegotiationFinishedSuccessfully];
-            [self decrementCounter];
-            
-            success();
-        }
-        else {
-            [self renegotiationFinished];
-            [self decrementCounter];
-            
-            failure();
-        }
+        
+        [self renegotiationFinished:stored];
+        
+        stored ? success() : failure();
     };
 }
 
 - (TokenErrorCallback)tokenFailureCallbackWithFailure:(NoArgsCallback)failure {
     return ^(RTOAuth2TokenError *tokenError) {
         DDLogDebug(@"failed refresh -- token error = %@", tokenError);
-        [self decrementCounter];
+        [self renegotiationFinished:NO];
     };
 };
 
